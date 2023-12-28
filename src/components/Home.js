54fde2f49e3b6e422d98319c5661afe706c604e5
slurpeesz <khtbee@gmail.com>
2023-12-28 12:22:11 -0800

    home page styling

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../index.css'
+
+const About = () => {
+  return (
+    <div
+      className="title"
+      style={{
+        background: "#F1F2EE",
+        color: "#646165",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div className='name'>
+        Kieu Be
+      </div>
+      <div className="aspiring">
+        <i>aspiring</i>
+      </div>
+      <div className='role'>
+        Front-End Developer
+      </div>
+    </div>
+  );
+}
+
+export default About
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../index.css'
+
+const Nav = () => {
+  return (
+    <div>
+      <ul>
+        <li>WORK</li>
+        <li>ABOUT</li>
+      </ul>
+
+    </div>
+  )
+}
+
+export default Nav
